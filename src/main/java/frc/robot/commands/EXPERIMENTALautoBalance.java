c2384421b3605558853d8cdafe4a78e128905bcc
FRC4069Driverstation <frc4069ds@gmail.com>
2023-02-02 16:48:04 -0500

    Added Auto Balance - not done

---
+++ src/main/java/frc/robot/commands/EXPERIMENTALautoBalance.java
@@ -0,0 +1,52 @@
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.SwerveSubsystem;
+
+
+/**
+ * A command to drive the robot with joystick input (passed in as {@link DoubleSupplier}s). Written
+ * explicitly for pedagogical purposes - actual code should inline a command this simple with {@link
+ * edu.wpi.first.wpilibj2.command.RunCommand}.
+ */
+public class EXPERIMENTALautoBalance extends CommandBase {
+  private final SwerveSubsystem m_drivebase;
+  private final Supplier<Boolean> autoBalanceON;
+
+
+  public EXPERIMENTALautoBalance(SwerveSubsystem drivebase, Supplier<Boolean> isActive) {
+    this.m_drivebase = drivebase;
+    this.autoBalanceON = isActive;
+
+    addRequirements(drivebase); // adds a requirement - if its not met then it will throw an error
+  }
+
+    @Override
+    public void execute() {
+    // what to do while this command is active
+    
+      /*
+       * 1. get current angle of the robot (or what direction the ramp is located )
+       * 2. From that figure out if you need to use the pitch or roll, or need to do the math for differnt angle of "climb"
+       * 3. tell robot to go forward at a set speed until a change in pitch or roll occurs
+       * 4. using that pitch or roll change as an error value for a pid loop gett the robot to balance 
+       */
+    SmartDashboard.putBoolean("Auto Balance", autoBalanceON.get());
+
+
+  }
+
+    @Override
+    public void end(boolean interrupted) {
+        // what to do at the end
+        m_drivebase.stopModules(); // stops wheels
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
+
--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -4,8 +4,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ExampleSubsystem;
 
-import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
 
 /**
  * A command to drive the robot with joystick input (passed in as {@link DoubleSupplier}s). Written
@@ -14,8 +14,7 @@ import java.util.function.DoubleSupplier;
  */
 public class ExampleCommand extends CommandBase {
   private final ExampleSubsystem m_ExampleSubsystem;
-  private final BooleanSupplier m_ExampleInput1;
-  private final BooleanSupplier m_ExampleInput2;
+  private final Supplier<Boolean> m_ExampleInput1, m_ExampleInput2;
 
   /**
    * Creates a new DefaultDrive.
@@ -24,10 +23,10 @@ public class ExampleCommand extends CommandBase {
    * @param forward The control input for driving forwards/backwards
    * @param rotation The control input for turning
    */
-  public ExampleCommand(ExampleSubsystem ExampleSubsystem, BooleanSupplier ExampleInput1, BooleanSupplier ExampleInput2) {
-    m_ExampleSubsystem = ExampleSubsystem;
-    m_ExampleInput1 = ExampleInput1;
-    m_ExampleInput2 = ExampleInput2;
+  public ExampleCommand(ExampleSubsystem ExampleSubsystem, Supplier<Boolean> ExampleInput1, Supplier<Boolean> ExampleInput2) {
+    this.m_ExampleSubsystem = ExampleSubsystem;
+    this.m_ExampleInput1 = ExampleInput1;
+    this.m_ExampleInput2 = ExampleInput2;
 
     addRequirements(ExampleSubsystem); // adds a requirement - if its not met then it will throw an error
   }
@@ -37,8 +36,8 @@ public class ExampleCommand extends CommandBase {
     // what to do while this command is active
     // this example will add or subtract 5 depenting on the input and print the current value
 
-    m_ExampleSubsystem.ExampleInput( m_ExampleInput1.getAsBoolean() ? 5 : 0);
-    m_ExampleSubsystem.ExampleInput( m_ExampleInput2.getAsBoolean() ? -5 : 0);
+    m_ExampleSubsystem.ExampleInput( m_ExampleInput1.get() ? 5 : 0);
+    m_ExampleSubsystem.ExampleInput( m_ExampleInput2.get() ? -5 : 0);
 
     SmartDashboard.putNumber("Example Output", m_ExampleSubsystem.ExampleOutput());
   }
