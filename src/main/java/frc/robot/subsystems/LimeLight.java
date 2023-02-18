61444733412bb3bfa7a1c0deb6f17d080d96194a
FlazeDug <dhruvj1323@gmail.com>
2023-02-18 14:49:04 -0500

    testing Git

--- src/main/java/frc/robot/subsystems/LimeLight.java
+++ src/main/java/frc/robot/subsystems/LimeLight.java
@@ -1,15 +1,57 @@
 package frc.robot.subsystems;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimeLight extends SubsystemBase{
     
     public PhotonCamera camera;
+    public PhotonPoseEstimator estimator;
+  
     
     public LimeLight(){
-       // camera = new PhotonCamera("LimeLight");
+        //camera = new PhotonCamera("LimeLight");
+    }
+
+    public Pose2d poseEstimation(){
+        return new Pose2d(0,0, new Rotation2d(0));
+
+    }
+
+    /**
+     * Get the Distance from the Limelight to the Refelctive tape on the Poles
+     * @return
+     */
+    public double refecltiveTapeDist(){
+        return 0;
+    }
+
+    /**
+     * Get the Angle from the Limelight to the Refelctive tape on the Poles
+     * @return
+     */
+    public double refecltiveTapeAngle(){
+        return 0;
+    }
+
+    /**
+     * Get the Distance from the Limelight to the Refelctive tape on the Poles
+     * @return
+     */
+    public double apriltagDist(){
+        return 0;
+    }
+    
+    /**
+     * Get the Angle from the Limelight to the Refelctive tape on the Poles
+     * @return
+     */
+    public double apriltagAngle(){
+        return 0;
     }
 
    
