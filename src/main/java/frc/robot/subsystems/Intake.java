6032dacb08822eb7446e481cc3b8c61649050c67
Ahmed Abdel-Dayem <97477649+Ahmed4069@users.noreply.github.com>
2023-02-18 15:01:14 -0500

    Intake stuff
    still working on it

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,25 +3,44 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase{
-    CANSparkMax intakeMotor;
+    private CANSparkMax intakeMotor;
+    private Encoder encoder;
+    private DigitalInput limitLeft, limitRight;
+    private double power; double speed; 
+    private double torque = 0, requestedTorque = 0;
 
+    public final int[] requiedTorque = {10, 20}; /**not exact values**/
     public Intake(){
-        intakeMotor = new CANSparkMax(Constants.INTAKE_ID, MotorType.kBrushless);
+        intakeMotor = new CANSparkMax(Constants.IntakeConstants.INTAKE_ID, MotorType.kBrushless);
+        encoder = new Encoder(Constants.IntakeConstants.ENCODER_ID_A, Constants.IntakeConstants.ENCODER_ID_A);
+        limitLeft = new DigitalInput(Constants.IntakeConstants.LIMIT_SWITCH_ID_1);
+        limitRight = new DigitalInput(Constants.IntakeConstants.LIMIT_SWITCH_ID_2);
+
+        power = 0;
+        torque = 0;
     }
 
-    public void update(Direction d, boolean withCameraInput){
+    public void update(Use d, boolean Cone){
+        requestedTorque = Cone ? requiedTorque[0] : requiedTorque[1];
 
+        intakeMotor.set(0.5);
+        power = 0.5;
+
+        torque = (9.5488 * power) / encoder.getRate();
+
+        SmartDashboard.putNumber("Intake Torque", torque);
     }
 
-    public enum Direction{ //might be helpful
-        IN_CONE,
-        OUT_CONE,
-        IN_CUBE,
-        OUT_CUBE
+    public enum Use{ //might be helpful
+       IN,
+       OUT
     }    
 }
 
