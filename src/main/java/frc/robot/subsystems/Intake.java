6e4e65a3043546fbc3287dd428dfa03169068498
FlazeDug <dhruvj1323@gmail.com>
2023-03-02 16:09:44 -0500

    AB

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -10,17 +10,16 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IO;
 import frc.robot.Constants.armAndIntakeConstants.intakeConstants;
 
-public class Intake extends SubsystemBase{
+public class Intake extends SubsystemBase {
     private CANSparkMax intake, wrist;
     private RelativeEncoder intakeEncoder, wristEncoder;
     public int controlType;
 
-    public Intake(){
+    public Intake() {
         controlType = 0;
 
-        intake= new CANSparkMax(intakeConstants.INTAKE_ID , MotorType.kBrushless);
-        wrist= new CANSparkMax(intakeConstants.WRIST_ID , MotorType.kBrushless);
-
+        intake = new CANSparkMax(intakeConstants.INTAKE_ID, MotorType.kBrushless);
+        wrist = new CANSparkMax(intakeConstants.WRIST_ID, MotorType.kBrushless);
 
         intake.setSmartCurrentLimit(30);
         wrist.setSmartCurrentLimit(40);
@@ -28,106 +27,102 @@ public class Intake extends SubsystemBase{
         intakeEncoder = intake.getEncoder();
 
         wrist.getOutputCurrent();
-        /* 
-         * wrist is 70:1 
-         * meaning 2940 encoder ticks : 1 wrist rotation 
-         *  1/2940 
+        /*
+         * wrist is 70:1
+         * meaning 2940 encoder ticks : 1 wrist rotation
+         * 1/2940
          */
 
-         wristEncoder.setPositionConversionFactor(47/70);
-
+        wristEncoder.setPositionConversionFactor(47 / 70);
 
     }
 
-    public void wristToPose(double position){
-        setWrist((position - getWristPose()*47) * intakeConstants.wristkP);
+    public void wristToPose(double position) {
+        setWrist((position - getWristPose() * 47) * intakeConstants.wristkP);
     }
+
     /**
      *
      * @param mode 1 : Manual controll, 2 : Parallel Lock, 3 : Perpendicular Lock
      * @return command to mode
-    */
-    public CommandBase setMode(int mode){
+     */
+    public CommandBase setMode(int mode) {
         return this.runOnce(() -> controlType = mode);
     }
-    
+
     /**
      * Set the speed of the wrist
+     * 
      * @param speed -iv is down and +iv is up
      */
-    public void setWrist(double speed){
+    public void setWrist(double speed) {
         wrist.set(speed);
     }
 
     /**
      * Set the speed of the intake
+     * 
      * @param speed -iv is inward and +iv is outward
      */
-    public void setIntake(double speed){
+    public void setIntake(double speed) {
         intake.set(speed);
     }
 
     /**
      * @return the current set speed of the wrist motor
      */
-    public double getWrist(){
+    public double getWrist() {
         return wrist.get();
     }
 
     /**
      * @return the current set speed of the intake motor
      */
-    public double getIntake(){
+    public double getIntake() {
         return intake.get();
     }
 
     /**
      * @return the current position of the wrist encoder
      */
-    public double getWristPose(){
+    public double getWristPose() {
         return wristEncoder.getPosition();
     }
-
 
     /**
      * @return the current position of the intake encoder
      */
-    public double getIntakePose(){
+    public double getIntakePose() {
         return intakeEncoder.getPosition();
     }
 
     /**
      * set position of the wrist encoder
+     * 
      * @param pose New position of the wrist encoder
      */
-    public void setWristPose(double pose){
+    public void setWristPose(double pose) {
         wristEncoder.setPosition(pose);
     }
 
-
     /**
      * set position of the intake encoder
+     * 
      * @param pose New position of the intake encoder
      */
-    public void setIntakePose(double pose){
+    public void setIntakePose(double pose) {
         intakeEncoder.setPosition(pose);
     }
 
-    public CommandBase zeroWrist(double pose){
+    public CommandBase zeroWrist(double pose) {
         return this.runOnce(() -> setWristPose(pose));
     }
-
-   
-
-
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
         SmartDashboard.putNumber("wrist Pose", getWristPose());
-        
+
     }
 
 }
-
-
