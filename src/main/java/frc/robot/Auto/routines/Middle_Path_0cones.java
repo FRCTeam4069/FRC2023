4dddbb13891c734f9688c68090273f510e46b76d
FRC4069Driverstation <frc4069ds@gmail.com>
2023-04-04 16:53:17 -0400

    auto wrok

--- src/main/java/frc/robot/Auto/routines/Middle_Path_0cones.java
+++ src/main/java/frc/robot/Auto/routines/Middle_Path_0cones.java
@@ -2,7 +2,6 @@ package frc.robot.Auto.routines;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Auto.Commands.armCommands.armToPose;
-import frc.robot.Auto.Commands.drivebaseCommands.autoBalance;
 
 /**
  * Interface for autonomous routines.
@@ -12,7 +11,6 @@ public class Middle_Path_0cones extends SequentialCommandGroup {
 
     public Middle_Path_0cones() {
         addCommands(new armToPose(-130, false, 5));
-
     }
 
 }
